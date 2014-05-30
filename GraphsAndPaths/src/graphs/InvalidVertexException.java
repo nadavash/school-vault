20ed9867acc0cbe28375a2df62b0149e6358e1b9
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2014-05-29 20:20:32 -0700

    Comment uncommented custom exception objects and commit showing path of one-step path for FindPaths

--- GraphsAndPaths/src/graphs/InvalidVertexException.java
+++ GraphsAndPaths/src/graphs/InvalidVertexException.java
@@ -1,5 +1,14 @@
 package graphs;
 
+/**
+ * @author Quynh Huynh, Nadav Ashkenazi
+ * @UWNetID quhuynh, nadava
+ * @studentID 1120428, 1230523
+ * @email quhuynh@uw.edu, nadava@uw.edu
+ * 
+ * Represents an exception for invalid vertex, specifically for edges
+ * with vertices that do not exist in a specific graph. 
+ */
 public class InvalidVertexException extends IllegalStateException {
 	// assign a version number for serializable class
 	 private static final long serialVersionUID = 1L;
--- GraphsAndPaths/src/graphs/NegativeWeightException.java
+++ GraphsAndPaths/src/graphs/NegativeWeightException.java
@@ -1,5 +1,14 @@
 package graphs;
 
+/**
+ * @author Quynh Huynh, Nadav Ashkenazi
+ * @UWNetID quhuynh, nadava
+ * @studentID 1120428, 1230523
+ * @email quhuynh@uw.edu, nadava@uw.edu
+ * 
+ * Represents an exception for negative weight edges. Edges in the Graph
+ * class can only have a cost of zero or more.
+ */
 public class NegativeWeightException extends IllegalStateException {
 	// assign a version number for serializable class
     private static final long serialVersionUID = 1L;
