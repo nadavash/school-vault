cbe148b2efa0ddb450095b5b10ef95abf4cb6bce
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2015-01-15 16:24:16 -0800

    Add front page and logo.

---
+++ info344/pa1/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NBA Players</title>
+</head>
+<body>
+	<div id="main">
+		<img src="../assets/nba-logo.jpg" alt="nba logo" />
+		<form method="GET" action="search.php">
+			<input type="text" name="q" />
+			<input type="submit" />
+		</form>
+	</div>
+</body>
+</html>
