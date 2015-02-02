bba816b06fef86729c7461f5de7904c6cec2e393
NadavAsh <ashkenazi.nadav@gmail.com>
2015-02-01 20:46:57 -0800

    Added fully functional trie and alphabet map.

---
+++ info344/pa2/LiveQuerySuggestions/LiveQuerySuggestions/AlphabetMap.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveQuerySuggestions
+{
+    public class AlphabetMap<T> : IEnumerable<T>
+    {
+        private T[] elements;
+        public int Count { get; set; }
+
+        public T this[char c]
+        {
+            get
+            {
+                return elements[c == ' ' ? 26 : c - 'a'];
+            }
+            set
+            {
+                int index = c == ' ' ? 26 : c - 'a';
+                if (elements[index] == null && value != null)
+                    ++Count;
+                else if (elements[index] != null && value == null)
+                    --Count;
+
+                elements[index] = value;
+            }
+        }
+
+        public AlphabetMap()
+        {
+            elements = new T[27];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new AlphabetMapEnumerator<T>(elements);
+        }
+
+        public class AlphabetMapEnumerator<Q> : IEnumerator<Q>
+        {
+            public int index;
+            public Q[] elements;
+
+            public AlphabetMapEnumerator(Q[] elements)
+            {
+                this.elements = elements;
+                index = -1;
+            }
+
+            public Q Current
+            {
+                get { return elements[index]; }
+            }
+
+            public bool MoveNext()
+            {
+                if (index < 0)
+                    index = 0;
+                else if (index < 27)
+                    ++index;
+
+                while (index < 27 && elements[index] == null)
+                {
+                    ++index;
+                }
+
+                return index < 27;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+            public void Dispose() { }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ info344/pa2/LiveQuerySuggestions/LiveQuerySuggestions/Trie.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace LiveQuerySuggestions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Trie
+    {
+        private TrieNode root;
+        private HashSet<string> set;
+
+        public Trie()
+        {
+            root = new TrieNode('\0');
+            set = new HashSet<string>();
+        }
+
+        public void ParseFromFile(StreamReader reader)
+        {
+            string word;
+            while ((word = reader.ReadLine()) != null)
+            {
+                Add(word);
+            }
+        }
+
+        public void Add(string word)
+        {
+            if (word == string.Empty)
+                return;
+
+            TrieNode current = root;
+            foreach (char c in word)
+            {
+                if (current.Children[c] == null)
+                {
+                    current.Children[c] = new TrieNode(c);
+                }
+                current = current.Children[c];
+            }
+            current.FullEntry = word;
+        }
+
+        public List<string> PrefixSearch(string prefix)
+        {
+            var matches = new List<string>();   
+            var path = new Stack<TrieNode>();
+            var current = root;
+            foreach (char c in prefix)
+            {
+                if (current.IsLeaf || current.Children[c] == null)
+                    break;
+
+                path.Push(current);
+                current = current.Children[c];
+            }
+
+
+            FindEntriesFrom(current, 5, matches);
+
+            return matches;
+        }
+
+        private void FindEntriesFrom(TrieNode node, int max, List<string> entries)
+        {
+            Queue<TrieNode> nodes = new Queue<TrieNode>();
+            nodes.Enqueue(node);
+
+            while (nodes.Count != 0 && entries.Count < max)
+            {
+                TrieNode current = nodes.Dequeue();
+                if (current.IsFullEntry)
+                    entries.Add(current.FullEntry);
+
+                if (!current.IsLeaf)
+                {
+                    foreach (TrieNode n in current.Children)
+                        nodes.Enqueue(n);
+                }
+            }
+        }
+
+        private class TrieNode
+        {
+            private AlphabetMap<TrieNode> children;
+
+            public char Key { get; set; }
+            public AlphabetMap<TrieNode> Children { 
+                get
+                {
+                    if (children == null)
+                        children = new AlphabetMap<TrieNode>();
+                    return children;
+                }
+            }
+            public string FullEntry { get; set; }
+            public bool IsFullEntry { get { return FullEntry != null; } }
+            public bool IsLeaf { get { return Children.Count == 0; } }
+
+            public TrieNode(char key)
+                : this(key, null) { }
+
+            public TrieNode(char key, string entry)
+            {
+                Key = key;
+                FullEntry = entry;
+            }
+        }
+    }
+}
