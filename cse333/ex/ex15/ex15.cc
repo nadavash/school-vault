2c10fb68f63e42af43c9c0dffda65b41dde31974
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2015-03-02 03:05:09 -0800

    ex15

---
+++ cse333/ex/ex15/ex15.cc
@@ -0,0 +1,74 @@
+// Copyright 2015, Nadav Ashkenazi
+// nadava@uw.edu - 1230523
+// CSE 333 15wi
+// Exercise 15
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/sendfile.h>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <netdb.h>
+
+// Prints the usage of the program.
+void PrintUsage(char* exeName) {
+  std::cerr << "Usage: ./" << exeName << " [hostname] [port] [local file]"
+            << std::endl;
+  exit(EXIT_FAILURE);
+}
+
+void WriteOut(int outFd, int inFd) {
+  off_t offset = 0;
+  while (true) {
+    ssize_t res = sendfile(outFd, inFd, &offset, 1);
+    if (res == 0) {
+      return
+    }
+    if (res == -1) {
+      if (errno == EAGAIN || errno == EINTR) continue;
+      break;
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  if (argc < 4) {
+    PrintUsage(argv[0]);
+  }
+
+  // Prepare local file
+  int fd = open(argv[3], O_RDONLY);
+  if (fd == -1) {
+    std::cerr << "Failed to open file '" << argv[3] << "'";
+    return EXIT_FAILURE;
+  }
+
+  // Prepare socket
+  struct addrinfo* ai;
+  struct addrinfo hints = {0};
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  int res = getaddrinfo(argv[1], argv[2], &hints, &ai);
+  if (res) {
+    std::cerr << "Failed to get addr info: " << res;
+    return EXIT_FAILURE;
+  }
+
+  // Make socket and connect
+  int soc = socket(a->ai_family, p->ai_socktype, p->ai_protocol);
+  if (soc < 0) {
+    std::cerr << "Failed to open socket: " << soc;
+    return EXIT_FAILURE;
+  }
+
+  if (connect(soc, a->ai_addr, a->ai_addrlen) == 0) {
+
+  } else {
+    std::cerr << "Faild to connect to server.";
+  }
+
+  freeaddrinfo(ai);
+  return EXIT_SUCCESS;
+}
